ffdb383fbcc13d498d5753826b4cc0a662eec167
Michael Herold <opensource@michaeljherold.com>
2020-03-27 10:31:55 -0500

    Do not use `xargs` to filter files
    Using `xargs` makes it so more than one JSON document gets produced if
    there are lints in more than one of the files. It also limits the
    parallelization that Rubocop can do.
    
    By explicitly naming the files that are changed, we can fix both of
    these issues.

--- spec/command_spec.rb
+++ spec/command_spec.rb
@@ -9,8 +9,14 @@ describe Command do
     it "returns built command" do
       command = Command.new(config).build
       expect(command).to eq(
-        "git diff origin/master --name-only --diff-filter=AM | xargs rubocop --parallel "\
-         "-f json --fail-level error -c .rubocop.yml --except Style/FrozenStringLiteralComment --force-exclusion"
+        "rubocop --parallel "\
+        "-f json "\
+        "--fail-level error "\
+        "-c .rubocop.yml "\
+        "--except Style/FrozenStringLiteralComment "\
+        "--force-exclusion "\
+        "-- "\
+        "$(git diff origin/master --name-only --diff-filter=AM)"
       )
     end
   end
